1c17f3cbde23e2c95d3c0e8862b4f6d551369997
Fernando Braga Alves <fbragaalves97@gmail.com>
2021-03-22 11:11:31 -0300

    basic estructure

---
+++ catalogoPDF.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * catalogoPDF
+ *
+ * @package           catalogoPDF
+ * @author            Fernando Braga Alves
+ * @copyright         2021 Fernando Braga Alves
+ * @license           GPL-3
+ *
+ * @wordpress-plugin
+ * Plugin Name:       catalogoPDF
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.7
+ * Requires PHP:      7.4
+ * Author:            Fernando Braga Alves
+ * License:           GPL v3
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
